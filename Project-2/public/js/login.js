954d2fa4a7dc19ea3f97c528353fb190af748c64
Sunny <loganayers2013@yahoo.com>
2019-09-25 13:50:25 -0500

    trying to push up changes before it crashes

--- Project-2/public/js/login.js
+++ Project-2/public/js/login.js
@@ -1,90 +1,103 @@
-$( document ).ready(function() {
-//Variables from page elements
-//-----------------------------
-//console.log("login.js checking in");
-// Login
-var $logEmail = $("log-email-form");
-var $logPassword = $("log-password-form");
-var $signUpButton = $("#signUpBttn");
-var $swapToSign = $("#swapToSignup");
+$(document).ready(function() {
+  //Variables from page elements
+  //-----------------------------
+  //console.log("login.js checking in");
+  // Login
+  var $logEmail = $("log-email-form");
+  var $logPassword = $("log-password-form");
+  var $logInButton = $("logInBttn");
+  var $swapToSign = $("#swapToSignup");
 
-// Signup
+  // Signup
 
-var $signEmail = $("sign-email-box");
-var $signPassword = $("sign-email-box");
-var $signUsername = $("sign-fullname-box");
-var $logInButton = $("#logInBttn");
-var $swapToLog = $("#swapToLogIn");
+  var $signEmail = $("#sign-email");
+  var $signPassword = $("#sign-password");
+  var $signUsername = $("#sign-username");
+  var $signUpButton = $("#signUpBttn");
+  var $swapToLog = $("#swapToLogIn");
 
-// Misc
-var $formSwap = $("#formSwap");
+  // Misc
+  var loggedIn = false;
 
-//Create the user object to be used in sign up page to save password
-var user = {
-  saveInfo: function(userInfo) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/userInfo",
-      data: JSON.stringify(userInfo)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/userInfo",
-      type: "GET"
-    });
-  }
-};
-
-
-var loginFormSubmit = function(event) {
-  event.preventDefault();
-
+  //Create the user object to be used in sign up page to save email, password, and username
   var user = {
-    email: $logEmail.val().trim(),
-    password: $logPassword.val().trim()
+    saveInfo: function(userInfo) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/Users",
+        data: JSON.stringify(userInfo)
+      });
+    },
+    getExamples: function() {
+      return $.ajax({
+        url: "api/Users",
+        type: "GET"
+      });
+    }
   };
 
-  if (!(user.email && user.password)) {
-    alert("You must enter a valid email and password.");
-    return;
+  var signUpFormSubmit = function(event) {
+    event.preventDefault();
+
+    var userInfo = {
+      email: $signEmail.val().trim(),
+      password: $signPassword.val().trim(),
+      username: $signUsername.val().trim()
+    };
+
+    if (!(userInfo.email && userInfo.password)) {
+      alert("You must enter a valid email and password.");
+      return;
+    } else {
+      user.saveInfo(userInfo).then(function(resp) {
+        console.log(resp);
+        console.log("User data updated.");
+      });
+    }
+  };
+
+  function loginValidate() {}
+
+  function logCheck() {
+    if (loggedIn === true) {
+      $("Form").hide();
+    }
   }
-  loginValidate();
 
-};
+  function swapToSign() {
+    $(".loginForm").hide();
+    $(".signInForm").show();
+  }
 
-function loginValidate(){}
+  function swapToLog() {
+    $(".loginForm").show();
+    $(".signInForm").hide();
+  }
 
+  function signUp(event) {
+    event.preventDefault();
+    var todo = {
+      text: $newItemInput.val().trim(),
+      complete: false
+    };
 
-function swapToSign(){
-  $(".loginForm").hide();
-  $(".signInForm").show();
-}
+    $.post("/api/todos", todo, getTodos);
+    $newItemInput.val("");
+  }
 
-function swapToLog(){
-  $(".loginForm").show();
-  $(".signInForm").hide();
-}
+  function logIn() {
+    console.log("logging you in.");
+    $("Form").hide();
+    var loggedIn = true;
+    logCheck();
+  }
 
-function signUp(){
-  
-  console.log("Signing you up.")
-  $("Form").hide();
-};
+  $swapToSign.on("click", swapToSign);
+  $swapToLog.on("click", swapToLog);
 
-function logIn(){
-  console.log("logging you in.")
-  $("Form").hide();
-};
-
-$swapToSign.on("click", swapToSign);
-$swapToLog.on("click", swapToLog);
-
-$signUpButton.on("click", signUp);
-$logInButton.on("click", logIn);
-
-
+  $signUpButton.on("click", signUpFormSubmit);
+  $logInButton.on("click", logIn);
 });
